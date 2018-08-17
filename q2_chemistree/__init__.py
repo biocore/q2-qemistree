db57bd5a015b78993cba24087008879cee7ccfaf
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2018-08-17 12:14:36 -0700

    Update __init__.py

--- q2_chemistree/__init__.py
+++ q2_chemistree/__init__.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2018, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 from ._version import get_versions
 
 __version__ = get_versions()['version']
