d11a7262828c5a48a83f5b92c0f061c6fe39adaa
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2018-10-04 14:13:53 -0700

    WIP: plugin setup

---
+++ q2_chemistree/_fingerprint.py
@@ -0,0 +1,4 @@
+from biom import Table
+
+def fingerprint(features: str, database: str=None) -> Table:
+    pass
---
+++ q2_chemistree/_hierarchy.py
@@ -0,0 +1,8 @@
+from skbio import TreeNode
+
+# something like this
+# str(representative_sequences.file.view(DNAFASTAFormat)
+
+
+def make_hierarchy(collated_fingerprints: str=None, threshold: float=0.5) -> TreeNode:
+    pass
