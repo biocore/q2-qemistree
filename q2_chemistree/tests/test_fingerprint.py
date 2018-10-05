60d1516f5a8b3a20f5b73720347014625e60ff92
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2018-10-05 16:45:51 -0700

    update path to sirius binaries

--- q2_chemistree/tests/test_fingerprint.py
+++ q2_chemistree/tests/test_fingerprint.py
@@ -2,14 +2,14 @@ import functools
 from unittest import TestCase, main
 import pandas as pd
 import os
-from q2_chemistree import fingerprint, collatefp, run_command
+from q2_chemistree import fingerprint, collatefp
 
 
 class fingerprintTests(TestCase):
     def setUp(self):
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         self.badsirpath = os.path.join(THIS_DIR, 'data/foo/bin')
-        self.goodsirpath = os.path.join(THIS_DIR, 'data/sirius-osx64-4.0/bin')
+        self.goodsirpath = os.path.join(THIS_DIR, 'data/sirius-osx64-4.0.1/bin')
         self.badionsfp = os.path.join(THIS_DIR, 'data/foo.mgf')
         self.goodionsfp = os.path.join(THIS_DIR, 'data/sirius.mgf')
         self.featureTable = os.path.join(THIS_DIR, 'data/features.csv')
@@ -19,12 +19,12 @@ class fingerprintTests(TestCase):
     def test_siriusBinaryPath(self):
         with self.assertRaises(OSError):
             fingerprint(self.badsirpath, self.goodionsfp, ppm_max=15,
-                        profile='orbitrap', processors=16)
+                        profile='orbitrap', n_jobs=1)
 
     def test_mgfPath(self):
         with self.assertRaises(OSError):
             fingerprint(self.goodsirpath, self.badionsfp, ppm_max=15,
-                        profile='orbitrap', processors=16)
+                        profile='orbitrap', n_jobs=1)
 
     def test_fingerprintOut(self):
         with self.assertRaises(RuntimeError):
@@ -39,7 +39,7 @@ class fingerprintTests(TestCase):
 
     def test_pipeline(self):
         tablefp = fingerprint(self.goodsirpath, self.goodionsfp, ppm_max=15,
-                              profile='orbitrap', processors=16)
+                              profile='orbitrap', n_jobs=1)
         features = pd.read_table(self.featureTable, sep=',', dtype=str)
         allfeatrs = set(features['row ID'])
         fpfeatrs = set(tablefp.to_dataframe().index)
