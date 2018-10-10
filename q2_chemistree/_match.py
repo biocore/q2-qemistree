e93382802af2ac5ec77b210d0e0b0de720d5dfad
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2018-10-10 11:24:18 -0700

    updating exception types

--- q2_chemistree/_match.py
+++ q2_chemistree/_match.py
@@ -25,8 +25,8 @@ def match_table(tree: TreeNode,
     feature_table : pandas dataframe
             MS1 feature table from MZmine2 with features in rows and
             samples in columns
-    Returns:
-    ----------
+    Returns
+    -------
     filtered_feature_table : pandas dataframe
         filtered MS1 feature table that contains only the features present in
         the tree
@@ -34,12 +34,12 @@ def match_table(tree: TreeNode,
     if feature_table.shape == (0, 0):
         raise ValueError("Cannot have empty feature table")
     if '#row ID' not in feature_table.columns:
-        raise RuntimeError('Feature table needs to be formatted correctly')
+        raise ValueError('Feature table needs to be formatted correctly')
     feature_table = feature_table.set_index('#row ID')
     allfeatrs = set(feature_table.index)
     tip_names = {node.name for node in tree.tips()}
     if not tip_names.issubset(allfeatrs):
-        raise RuntimeError('One or more tree tips not in the feature table')
+        raise ValueError('One or more tree tips not in the feature table')
     common_features = list(allfeatrs.intersection(tip_names))
     filtered_feature_table = feature_table.loc[common_features]
 
