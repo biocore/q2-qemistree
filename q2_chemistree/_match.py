4d9ded4f8ed970579a373d3275fc9e7e62430247
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2018-10-10 18:27:34 -0700

    changing input ot biom+associated changes in code

--- q2_chemistree/_match.py
+++ q2_chemistree/_match.py
@@ -9,10 +9,11 @@
 import biom
 import pandas as pd
 from skbio import TreeNode
+from biom.table import Table
 
 
 def match_table(tree: TreeNode,
-                feature_table: pd.DataFrame) -> biom.Table:
+                feature_table: biom.Table) -> biom.Table:
     '''
     This function filters the feature table to retain the features present in
         the tree
@@ -22,35 +23,31 @@ def match_table(tree: TreeNode,
     tree : TreeNode
         skbio TreeNode object representing tree of relatedness
         between molecules
-    feature_table : pandas dataframe
-            MS1 feature table from MZmine2 with features in rows and
-            samples in columns
+    feature_table : pd.DataFrame
+        feature table with features in columns and samples in rows
 
     Raises
     ------
     ValueError
         If ``feature_table`` has no features
-        If ``feature_table`` does not have a column named '#row ID'
         If ``tree`` tips are not a subset of feature names in ``feature_table``
 
     Returns
     -------
-    filtered_feature_table : pandas dataframe
-        filtered MS1 feature table that contains only the features present in
+    biom.Table
+        filtered feature table that contains only the features present in
         the tree
-        Raises
-
     '''
-    if feature_table.empty:
-        raise ValueError("Cannot have empty feature table")
-    if '#row ID' not in feature_table.columns:
-        raise ValueError('Feature table needs to be formatted correctly')
-    feature_table = feature_table.set_index('#row ID')
-    allfeatrs = set(feature_table.index)
+    if feature_table.shape[0] == 0:
+        raise ValueError("There are no features in the feature table!")
+    allfeatrs = set(feature_table.ids(axis='observation'))
     tip_names = {node.name for node in tree.tips()}
     if not tip_names.issubset(allfeatrs):
-        raise ValueError('One or more tree tips not in the feature table')
+        extra_tips = tip_names-tip_names.intersection(allfeatrs)
+        raise ValueError('Some tips were not in the feature table:\n')
+        print(extra_tips)
     common_features = list(allfeatrs.intersection(tip_names))
-    filtered_feature_table = feature_table.loc[common_features]
-
+    filtered_feature_table = feature_table.filter(common_features,
+                                                  axis='observation',
+                                                  inplace=False)
     return filtered_feature_table
