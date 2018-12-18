62a1efc9a4fac39050147ef966a9d834d7cfe944
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2018-12-17 16:33:26 -0800

    adding function to filter predicted properties

--- q2_chemistree/_collate_fingerprint.py
+++ q2_chemistree/_collate_fingerprint.py
@@ -10,18 +10,26 @@ import os
 import biom
 import pandas as pd
 import numpy as np
+import pkg_resources
 
 from ._semantics import CSIDirFmt
 
 
-def collate_fingerprint(csi_result: CSIDirFmt) -> biom.Table:
+data = pkg_resources.resource_filename('q2_chemistree', 'data')
+
+
+def collate_fingerprint(csi_result: CSIDirFmt,
+                        keep_pubchem: bool = True) -> biom.Table:
     '''
     This function collates chemical fingerprints for mass-spec
     features in an experiment.
 
     Parameters
     ----------
-    csi_result : CSI:FingerID output folder
+    csi_result : CSIDirFmt
+        CSI:FingerID output folder
+    keep_pubchem : bool
+        flag to filter molecular properties to keep only PUBCHEM fingerprints
 
     Raises
     ------
@@ -35,7 +43,7 @@ def collate_fingerprint(csi_result: CSIDirFmt) -> biom.Table:
         and molecular substructure IDs (as samples).
     '''
     if isinstance(csi_result, CSIDirFmt):
-        csi_result = str(csi_result.path)
+        csi_result = str(csi_result.get_path())
 
     fpfoldrs = os.listdir(csi_result)
     molfp = dict()
@@ -48,15 +56,24 @@ def collate_fingerprint(csi_result: CSIDirFmt) -> biom.Table:
                 with open(os.path.join(fidpath, 'fingerprints', fname)) as f:
                     fp = f.read().strip().split('\n')
                 molfp[fid] = fp
-
     fingerids = pd.DataFrame.from_dict(molfp, orient='index')
     if fingerids.shape == (0, 0):
         raise ValueError('Fingerprint file is empty!')
+    substructrs = pd.read_table(os.path.join(csi_result, 'fingerprints.csv'),
+                                index_col='absoluteIndex')
     fingerids.index.name = '#featureID'
+    fingerids.columns = substructrs.index
+    if keep_pubchem:
+        fingerids = select_pubchem(fingerids)
     npfid = np.asarray(fingerids)
-
     # biom requires that ids be strings
     fptable = biom.table.Table(data=npfid,
                                observation_ids=fingerids.index.astype(str),
                                sample_ids=fingerids.columns.astype(str))
     return fptable
+
+def select_pubchem(fingerids):
+    properties = pd.read_table(os.path.join(data, 'molecular_properties.csv'),
+                               index_col='absoluteIndex')
+    pubchem_indx =  list(properties.loc[properties.type=='PUBCHEM'].index)
+    return fingerids[pubchem_indx]
