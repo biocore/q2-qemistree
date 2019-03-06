3de5ff2e4d7afa37774534a3bc2392496732a4f9
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-03-06 13:01:34 -0800

    modifying tests for md5 pipleine

--- q2_chemistree/tests/test_match_table.py
+++ q2_chemistree/tests/test_match_table.py
@@ -11,40 +11,36 @@ import os
 import warnings
 from biom import load_table
 from biom.table import Table
-from q2_chemistree import match_table, collate_fingerprint, make_hierarchy
+
+from q2_chemistree._collate_fingerprint import collate_fingerprint
+from q2_chemistree._match import match_label
 
 
 class test_match(TestCase):
     def setUp(self):
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         table = Table({}, [], [])
-        self.emptyfeatures = table
+        self.emptyfps = table
         table = Table({}, ['a', 'b', 'c'], [])
         self.wrongtips = table
-        self.goodtable = os.path.join(THIS_DIR, 'data/features_formated.biom')
-        self.goodcsi = os.path.join(THIS_DIR, 'data/goodcsi')
-        self.goodthresh = 0.5
-        tablefp = collate_fingerprint(self.goodcsi)
-        treeout = make_hierarchy(tablefp, prob_threshold=self.goodthresh)
-        self.goodtree = treeout
+        goodtable = os.path.join(THIS_DIR, 'data/features_formated.biom')
+        self.features = load_table(goodtable)
+        goodcsi = os.path.join(THIS_DIR, 'data/goodcsi')
+        self.tablefp = collate_fingerprint(goodcsi)
 
     def test_emptyTable(self):
         with self.assertRaises(ValueError):
-            match_table(self.goodtree, self.emptyfeatures)
+            match_label(self.emptyfps, self.features)
 
     def test_tipMismatch(self):
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            match_table(self.goodtree, self.wrongtips)
-            assert len(w) == 1
-            assert issubclass(w[-1].category, UserWarning)
+        with self.assertRaises(ValueError):
+            match_label(self.wrongtips, self.features)
 
     def test_matchPipeline(self):
-        tips = {node.name for node in self.goodtree.tips()}
-        features = load_table(self.goodtable)
-        tableout = match_table(self.goodtree, features)
-        tableids = set(tableout.ids(axis='observation'))
-        self.assertEqual(tips, tableids)
+        relabel_fps, matched_table = match_label(self.tablefp, self.features)
+        featrs = set(matched_table.ids(axis='observation'))
+        fps = set(relabel_fps.index)
+        self.assertEqual(fps, featrs)
 
 
 if __name__ == '__main__':
