9e9e26c1d4330a5d59b10094b68744df4c2bc66d
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-03-06 12:59:30 -0800

    change dataframe to dense to allow modification

--- q2_chemistree/_match.py
+++ q2_chemistree/_match.py
@@ -15,34 +15,34 @@ import numpy as np
 def match_label(collated_fingerprints: biom.Table, feature_table: biom.Table):
     '''
     This function filters the feature table to retain only features with
-    fingerprints. It also relabels with features with md5 hash of its
+    fingerprints. It also relabels features with md5 hash of its
     binary fingerprint vector.
     '''
-    fingerprints = collated_fingerprints.to_dataframe()
-    if fingerprints.shape == (0, 0):
+    fps = collated_fingerprints.to_dataframe(dense=True)
+    allfps = set(fps.index)
+    if fps.shape == (0, 0):
         raise ValueError("Cannot have empty fingerprint table")
-    table = feature_table.to_dataframe()
-    fps = set(fingerprints.index)
+    table = feature_table.to_dataframe(dense=True)
     allfeatrs = set(table.index)
-    if not fps.issubset(allfeatrs):
-        extra_tips = fps-fps.intersection(allfeatrs)
-        warnings.warn(UserWarning('The following tips were not '
-                                  'found in the feature table:\n' +
-                                  ', '.join([str(i) for i in extra_tips])))
-    filtered_table = table.reindex(fps)
-    fingerprints = (fingerprints > 0.5).astype(int)
-    for fid in fingerprints.index:
-        md5 = hashlib.sha256(fingerprints[fid].values.tobytes()).hexdigest()
-        fingerprints.loc[fid, 'label'] = md5
+    if not allfps.issubset(allfeatrs):
+        extra_tips = allfps-allfps.intersection(allfeatrs)
+        raise ValueError('The following tips were not '
+                         'found in the feature table:\n' +
+                         ', '.join([str(i) for i in extra_tips]))
+    filtered_table = table.reindex(allfps)
+    fps = (fps > 0.5).astype(int)
+    for fid in fps.index:
+        md5 = hashlib.sha256(fps.loc[fid].values.tobytes()).hexdigest()
+        fps.loc[fid, 'label'] = md5
         filtered_table.loc[fid, 'label'] = md5
-    relabeled_fingerprints = fingerprints.groupby(fingerprints.label).first()
+    relabel_fps = fps.groupby(fps.label).first()
     matched_table = filtered_table.groupby(filtered_table.label).sum()
-    npfid = np.asarray(matched_table)
     # biom requires that ids be strings
-    matched_table = biom.table.Table(data=npfid,
+    npfeatures = np.asarray(matched_table)
+    matched_table = biom.table.Table(data=npfeatures,
                                      observation_ids=
                                      matched_table.index.astype(str),
                                      sample_ids=
                                      matched_table.columns.astype(str))
 
-    return relabeled_fingerprints, matched_table
+    return relabel_fps, matched_table
