9c6e1ba5017fda077d75b7daf56e7f5b862a0078
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-03-06 12:58:30 -0800

    add imports and quotes arounf jaccard

--- q2_chemistree/_hierarchy.py
+++ q2_chemistree/_hierarchy.py
@@ -13,9 +13,9 @@ from scipy.spatial.distance import squareform
 from scipy.cluster.hierarchy import linkage
 from skbio import TreeNode
 
-from ._collate_fingerprint import collate_fingerprint
-from ._match import match_label
-from ._semantics import CSIDirFmt
+from q2_chemistree._collate_fingerprint import collate_fingerprint
+from q2_chemistree._match import match_label
+from q2_chemistree._semantics import CSIDirFmt
 
 
 def build_tree(relabeled_fingerprints: pd.DataFrame) -> TreeNode:
@@ -24,7 +24,7 @@ def build_tree(relabeled_fingerprints: pd.DataFrame) -> TreeNode:
     features using molecular substructure fingerprints.
     '''
     distmat = pairwise_distances(X=relabeled_fingerprints,
-                                 Y=None, metric=jaccard)
+                                 Y=None, metric='jaccard')
     distsq = squareform(distmat, checks=False)
     linkage_matrix = linkage(distsq, method='average')
     tree = TreeNode.from_linkage_matrix(linkage_matrix,
