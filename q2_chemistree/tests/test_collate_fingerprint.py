ebe023ced218d54f4094219b9b9f7176955aeef6
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-04-02 16:47:54 -0700

    changing import line number

--- q2_chemistree/tests/test_collate_fingerprint.py
+++ q2_chemistree/tests/test_collate_fingerprint.py
@@ -12,8 +12,8 @@ import pandas as pd
 import os
 import pkg_resources
 import qiime2
-from q2_chemistree import CSIDirFmt
 
+from q2_chemistree import CSIDirFmt
 from q2_chemistree._collate_fingerprint import collate_fingerprint
 
 data = pkg_resources.resource_filename('q2_chemistree', 'data')
