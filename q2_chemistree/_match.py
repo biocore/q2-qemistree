9cc421c0b166cbf9e0bf6453e4a8c0008811e9c0
Tobin Groth <tgroth@ucsd.edu>
2019-04-23 14:20:17 -0700

    prep for switch to biom

--- q2_chemistree/_match.py
+++ q2_chemistree/_match.py
@@ -50,31 +50,32 @@ def match_label(collated_fingerprints: pd.DataFrame,
     allfps = list(fps.index)
     if fps.empty:
         raise ValueError("Cannot have empty fingerprint table")
-    table = feature_table.to_dataframe(dense=True)
-    allfeatrs = set(table.index)
-    if not set(allfps).issubset(allfeatrs):
-        extra_tips = set(allfps) - set(allfps).intersection(allfeatrs)
-        raise ValueError('The following tips were not '
-                         'found in the feature table:\n' +
-                         ', '.join([str(i) for i in extra_tips]))
-    filtered_table = table.reindex(allfps)
-    fps = (fps > 0.5).astype(int)
-    list_md5 = []
-    for fid in allfps:
-        md5 = str(hashlib.md5(fps.loc[fid].values.tobytes()).hexdigest())
-        list_md5.append(md5)
-    fps['label'] = list_md5
-    filtered_table['label'] = list_md5
-    feature_data = pd.DataFrame(columns=['label', '#featureID'])
-    feature_data['label'] = list_md5
-    feature_data['#featureID'] = allfps
-    feature_data.set_index('label', inplace=True)
-    relabel_fps = fps.groupby('label').first()
-    matched_table = filtered_table.groupby('label').sum()
-    # biom requires that ids be strings
-    npfeatures = matched_table.values
-    matched_table = biom.table.Table(
-        data=npfeatures, observation_ids=matched_table.index.astype(str),
-        sample_ids=matched_table.columns.astype(str))
+    # TODO:  use biom instead of pandas for feature table
+    # table = feature_table.to_dataframe(dense=True)
+    # allfeatrs = set(table.index)
+    # if not set(allfps).issubset(allfeatrs):
+    #     extra_tips = set(allfps) - set(allfps).intersection(allfeatrs)
+    #     raise ValueError('The following tips were not '
+    #                      'found in the feature table:\n' +
+    #                      ', '.join([str(i) for i in extra_tips]))
+    # filtered_table = table.reindex(allfps)
+    # fps = (fps > 0.5).astype(int)
+    # list_md5 = []
+    # for fid in allfps:
+    #     md5 = str(hashlib.md5(fps.loc[fid].values.tobytes()).hexdigest())
+    #     list_md5.append(md5)
+    # fps['label'] = list_md5
+    # filtered_table['label'] = list_md5
+    # feature_data = pd.DataFrame(columns=['label', '#featureID'])
+    # feature_data['label'] = list_md5
+    # feature_data['#featureID'] = allfps
+    # feature_data.set_index('label', inplace=True)
+    # relabel_fps = fps.groupby('label').first()
+    # matched_table = filtered_table.groupby('label').sum()
+    # # biom requires that ids be strings
+    # npfeatures = matched_table.values
+    # matched_table = biom.table.Table(
+    #     data=npfeatures, observation_ids=matched_table.index.astype(str),
+    #     sample_ids=matched_table.columns.astype(str))
 
     return relabel_fps, matched_table, feature_data
