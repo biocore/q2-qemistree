93d54f81178545ad79c17e6557f610c083d743f0
Mingxun Wang <mwang87@gmail.com>
2019-05-17 15:12:45 -0700

    removing debugging outputs

--- q2_chemistree/_summarize.py
+++ q2_chemistree/_summarize.py
@@ -18,5 +18,4 @@ from ._semantics import CSIDirFmt
 def output_fingerprints(csi_results: CSIDirFmt) -> pd.DataFrame:
     all_fingerprints = collate_fingerprint(csi_results, False)
     all_fingerprints["#FeatureID"] = all_fingerprints.index
-    print(all_fingerprints.head())
     return all_fingerprints
