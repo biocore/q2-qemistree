a5a604a9ee97e9f5de9d53e8b898ac999ff46fe2
Tobin Groth <tgroth@Tobins-MacBook-Pro.local>
2019-05-31 13:42:30 -0700

    Finished with flake8 for maximum style points

--- q2_qemistree/_match.py
+++ q2_qemistree/_match.py
@@ -50,39 +50,29 @@ def match_label(collated_fingerprints: pd.DataFrame,
     allfps = list(fps.index)
     if fps.empty:
         raise ValueError("Cannot have empty fingerprint table")
-    # TODO:  use biom instead of pandas for feature table
-    # table = feature_table.to_dataframe(dense=True)
-    # allfeatrs = set(table.index)
     allfeatrs = set(feature_table.ids('observation'))
     if not set(allfps).issubset(allfeatrs):
-    	extra_tips = set(allfps) - set(allfps).intersection(allfeatrs)
+        extra_tips = set(allfps) - set(allfps).intersection(allfeatrs)
         raise ValueError('The following tips were not '
-                          'found in the feature table:\n' +
-                          ', '.join([str(i) for i in extra_tips]))
-    # filtered_table = table.reindex(allfps)
-    filtered_data = [feature_table.data(i,axis='observation') for i in allfps]
+                         'found in the feature table:\n' +
+                         ', '.join([str(i) for i in extra_tips]))
+    filtered_data = [feature_table.data(i, axis='observation') for i in allfps]
     fps = (fps > 0.5).astype(int)
     list_md5 = []
     for fid in allfps:
-       md5 = str(hashlib.md5(fps.loc[fid].values.tobytes()).hexdigest())
-       list_md5.append(md5)
+        md5 = str(hashlib.md5(fps.loc[fid].values.tobytes()).hexdigest())
+        list_md5.append(md5)
     fps['label'] = list_md5
-    # filtered_table['label'] = list_md5
-    # feature_data = pd.DataFrame(columns=['label', '#featureID'])
-    # feature_data['label'] = list_md5
-    # feature_data['#featureID'] = allfps
-    # feature_data.set_index('label', inplace=True)
+    feature_data = pd.DataFrame(columns=['label', '#featureID'])
+    feature_data['label'] = list_md5
+    feature_data['#featureID'] = allfps
+    feature_data.set_index('label', inplace=True)
     relabel_fps = fps.groupby('label').first()
     relabeled_table = biom.table.Table(
-    	filtered_data, allfps, features.ids('sample'), [{'label': str(val)} for val in list_md5])
-    bin_f = lambda id_, x: x['label']
-	collapsed_table = relabeled_table.collapse(bin_f, norm=True, 
-    	min_group_size=1,axis='observation').sort(axis='observation')
-    # matched_table = filtered_table.groupby('label').sum()
-    # # biom requires that ids be strings
-    # npfeatures = matched_table.values
-    # matched_table = biom.table.Table(
-    #     data=npfeatures, observation_ids=matched_table.index.astype(str),
-    #     sample_ids=matched_table.columns.astype(str))
+        filtered_data, allfps, feature_table.ids('sample'),
+        [{'label': str(val)} for val in list_md5])
 
+    def bin_f(id_, x): return x['label']
+    collapsed_table = relabeled_table.collapse(
+                      bin_f, norm=True, min_group_size=1, axis='observation')
     return relabel_fps, collapsed_table, feature_data
