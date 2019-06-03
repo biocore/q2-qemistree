31e903ec07ea1f2bfcec0da2492ec61f8ba97c7b
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-06-03 15:17:27 -0700

    add tests for feature data

--- q2_qemistree/tests/test_match_table.py
+++ q2_qemistree/tests/test_match_table.py
@@ -12,7 +12,9 @@ import pandas as pd
 from biom import load_table
 
 from q2_qemistree._collate_fingerprint import collate_fingerprint
-from q2_qemistree._match import match_label
+from q2_qemistree._match import match_tables
+from q2_qemistree._semantics import CSIDirFmt
+from q2_qemistree._transformer import _read_dataframe
 
 
 class TestMatch(TestCase):
@@ -22,32 +24,45 @@ class TestMatch(TestCase):
         self.emptyfps = table
         table = pd.DataFrame(index=['a', 'b', 'c'], data=['a', 'b', 'c'])
         self.wrongtips = table
-        goodtable = os.path.join(THIS_DIR, 'data/features_formated.biom')
+        goodtable = os.path.join(THIS_DIR, 'data/feature_table1.biom')
         self.features = load_table(goodtable)
-        goodcsi = os.path.join(THIS_DIR, 'data/goodcsi')
+        goodfdata = os.path.join(THIS_DIR, 'data/feature_data1.txt')
+        self.fdata = _read_dataframe(goodfdata)
+        wrongfdata = os.path.join(THIS_DIR, 'data/feature_data2.txt')
+        self.wrongfdata = _read_dataframe(wrongfdata)
+        missingfdata = os.path.join(THIS_DIR, 'data/fdata1_missing.txt')
+        self.missingfdata = _read_dataframe(missingfdata)
+        goodcsi = os.path.join(THIS_DIR, 'data/goodcsi1')
         self.tablefp = collate_fingerprint(goodcsi)
 
-    def test_emptyTable(self):
-        msg = "Cannot have empty fingerprint table"
+    def test_missingFdata(self):
+        msg = "Feature data does not contain 'row m/z'"
         with self.assertRaisesRegex(ValueError, msg):
-            match_label(self.emptyfps, self.features)
+            match_tables(self.tablefp, self.features, self.missingfdata)
+
+    def test_matchFtableFdata(self):
+        msg = "The identifiers in feature data and feature table do not match"
+        with self.assertRaisesRegex(ValueError, msg):
+            match_tables(self.tablefp, self.features, self.wrongfdata)
 
     def test_tipMismatch(self):
         msg = "^The following tips were not found in the feature table:"
         with self.assertRaisesRegex(ValueError, msg):
-            match_label(self.wrongtips, self.features)
+            match_tables(self.wrongtips, self.features, self.fdata)
 
     def test_matchFdata(self):
-        relabeled_fps, matched_ft, matched_fdata = match_label(self.tablefp,
-                                                               self.features)
+        relabeled_fps, matched_ft, matched_fdata = match_tables(self.tablefp,
+                                                                self.features,
+                                                                self.fdata)
         fdata_featrs = sorted(list(matched_fdata.index))
-        featrs = sorted(list(matched_ft.ids(axis='observation')))
+        featrs = sorted(list(matched_ft.index))
         self.assertEqual(fdata_featrs, featrs)
 
     def test_matchFps(self):
-        relabeled_fps, matched_ft, matched_fdata = match_label(self.tablefp,
-                                                               self.features)
-        featrs = sorted(list(matched_ft.ids(axis='observation')))
+        relabeled_fps, matched_ft, matched_fdata = match_tables(self.tablefp,
+                                                                self.features,
+                                                                self.fdata)
+        featrs = sorted(list(matched_ft.index))
         fps = sorted(list(relabeled_fps.index))
         self.assertEqual(fps, featrs)
 
