5e34d9ee2e61e9d1910454ed1075bc864975e243
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-06-03 17:43:51 -0700

    adding feature data in tests

--- q2_qemistree/tests/test_make_hierarchy.py
+++ q2_qemistree/tests/test_make_hierarchy.py
@@ -9,14 +9,13 @@
 from unittest import TestCase, main
 import os
 import qiime2
-import pandas as pd
 from biom.table import Table
 from biom import load_table
 from q2_qemistree import make_hierarchy
 from q2_qemistree import CSIDirFmt
 
 from q2_qemistree._collate_fingerprint import collate_fingerprint
-from q2_qemistree._hierarchy import merge_feature_data
+from q2_qemistree._transformer import _read_dataframe
 
 
 class TestHierarchy(TestCase):
@@ -24,69 +23,69 @@ class TestHierarchy(TestCase):
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         tablefp = Table({}, [], [])
         self.emptyfeatures = tablefp
-        goodtable = os.path.join(THIS_DIR, 'data/features_formated.biom')
+        goodtable = os.path.join(THIS_DIR, 'data/feature_table1.biom')
         self.features = load_table(goodtable)
-        goodtable = os.path.join(THIS_DIR, 'data/features2_formated.biom')
+        goodfdata = os.path.join(THIS_DIR, 'data/feature_data1.txt')
+        self.fdata = _read_dataframe(goodfdata)
+        goodtable = os.path.join(THIS_DIR, 'data/feature_table2.biom')
         self.features2 = load_table(goodtable)
+        goodfdata = os.path.join(THIS_DIR, 'data/feature_data2.txt')
+        self.fdata2 = _read_dataframe(goodfdata)
         self.goodcsi = qiime2.Artifact.load(os.path.join(THIS_DIR,
-                                                         'data/csiFolder.qza'))
+                                                         'data/goodcsi1.qza'))
         goodcsi = self.goodcsi.view(CSIDirFmt)
         self.collated = collate_fingerprint(goodcsi)
         self.goodcsi2 = qiime2.Artifact.load(os.path.join(
-                                            THIS_DIR, 'data/csiFolder2.qza'))
+                                            THIS_DIR, 'data/goodcsi2.qza'))
         goodcsi = self.goodcsi2.view(CSIDirFmt)
         self.collated2 = collate_fingerprint(goodcsi)
 
-    def test_unequal_inputs(self):
+    def test_unequal_inputs1(self):
         goodcsi = self.goodcsi.view(CSIDirFmt)
         msg = ("The feature tables and CSI results should have a one-to-one"
                " correspondance.")
         with self.assertRaisesRegex(ValueError, msg):
-            make_hierarchy([goodcsi], [self.features, self.features2])
+            make_hierarchy([goodcsi], [self.features, self.features2],
+                           [self.fdata, self.fdata2])
+
+    def test_unequal_inputs2(self):
+        goodcsi = self.goodcsi.view(CSIDirFmt)
+        goodcsi2 = self.goodcsi2.view(CSIDirFmt)
+        msg = ("The feature tables and feature data should have a one-to-one"
+               " correspondance.")
+        with self.assertRaisesRegex(ValueError, msg):
+            make_hierarchy([goodcsi, goodcsi2],
+                           [self.features, self.features2], [self.fdata])
 
     def test_mergeFeatureDataSingle(self):
         goodcsi1 = self.goodcsi.view(CSIDirFmt)
         treeout, merged_fts, merged_fdata = make_hierarchy(
-            [goodcsi1], [self.features])
+            [goodcsi1], [self.features], [self.fdata])
         featrs = sorted(list(merged_fts.ids(axis='observation')))
         fdata_featrs = sorted(list(merged_fdata.index))
-        self.assertEqual(len(featrs) == 3, True)
+        self.assertEqual(len(featrs) == 5, True)
         self.assertEqual(fdata_featrs, featrs)
 
     def test_mergeFeatureDataMultiple(self):
         goodcsi1 = self.goodcsi.view(CSIDirFmt)
         goodcsi2 = self.goodcsi2.view(CSIDirFmt)
         treeout, merged_fts, merged_fdata = make_hierarchy(
-            [goodcsi1, goodcsi2], [self.features, self.features2])
+            [goodcsi1, goodcsi2], [self.features, self.features2],
+            [self.fdata, self.fdata2])
         featrs = sorted(list(merged_fts.ids(axis='observation')))
         fdata_featrs = sorted(list(merged_fdata.index))
-        self.assertEqual(len(featrs) == 9, True)
+        self.assertEqual(len(featrs) == 11, True)
         self.assertEqual(fdata_featrs, featrs)
-
-    def test_FeatureDataMultipleRepeated(self):
-        fdata1 = pd.DataFrame(index=list('aabbc'),
-                              data=[['1', '1'], ['1', '2'], ['1', '3'],
-                              ['1', '4'], ['1', '5']],
-                              columns=['table_number', '#featureID'])
-        fdata2 = pd.DataFrame(index=list('accdef'),
-                              data=[['2', '1'], ['2', '2'], ['2', '3'],
-                              ['2', '4'], ['2', '5'], ['2', '6']],
-                              columns=['table_number', '#featureID'])
-        merged_fdata = merge_feature_data([fdata1, fdata2])
-        fdata_featrs = sorted(list(merged_fdata.index))
-        self.assertEqual(merged_fdata.loc['a', 'table_number'], '1,1,2')
-        self.assertEqual(merged_fdata.loc['c', 'table_number'], '1,2,2')
-        self.assertEqual(fdata_featrs, ['a', 'b', 'c', 'd', 'e', 'f'])
 
     def test_emptyFeatures(self):
         goodcsi = self.goodcsi.view(CSIDirFmt)
         with self.assertRaises(ValueError):
-            make_hierarchy([goodcsi], [self.emptyfeatures])
+            make_hierarchy([goodcsi], [self.emptyfeatures], [self.fdata])
 
     def test_tipMatchSingle(self):
         goodcsi = self.goodcsi.view(CSIDirFmt)
         treeout, feature_table, merged_fdata = make_hierarchy(
-            [goodcsi], [self.features])
+            [goodcsi], [self.features], [self.fdata])
         tip_names = {node.name for node in treeout.tips()}
         self.assertEqual(tip_names, set(feature_table._observation_ids))
 
@@ -94,7 +93,8 @@ class TestHierarchy(TestCase):
         goodcsi1 = self.goodcsi.view(CSIDirFmt)
         goodcsi2 = self.goodcsi2.view(CSIDirFmt)
         treeout, merged_fts, merged_fdata = make_hierarchy(
-            [goodcsi1, goodcsi2], [self.features, self.features2])
+            [goodcsi1, goodcsi2], [self.features, self.features2],
+            [self.fdata, self.fdata2])
         tip_names = {node.name for node in treeout.tips()}
         self.assertEqual(tip_names, set(merged_fts._observation_ids))
 
