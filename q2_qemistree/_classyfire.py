d4ad80e7d7b862d6f62a8aa39075972bff85e6bc
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-10-14 16:51:32 -0700

    add warning about unexpected server responses, parse status code 200 only

--- q2_qemistree/_classyfire.py
+++ q2_qemistree/_classyfire.py
@@ -8,45 +8,39 @@
 
 import requests
 import pandas as pd
+import warnings
 
 
-def get_classyfire_taxonomy(feature_data: pd.DataFrame,
-                            classyfire_levels: list = None) -> pd.DataFrame:
+def get_classyfire_taxonomy(feature_data: pd.DataFrame) -> pd.DataFrame:
     '''This function uses structural predictions of molecules (SMILES)
     to run Classyfire and obtain chemical taxonomy for each mass-spec feature.
     It appends chemical taxonomy of features to feature data table.
+
     Parameters
     ----------
     feature_data : pd.DataFrame
         table that maps MD5 hash of mass-spec features to their structural
         annotations (smiles)
-    feature_table : list
-        list of chemical taxonomic levels to obtain from Classyfire.
-        Default: ['kingdom', 'superclass', 'class','subclass',
-                  'direct_parent']
+
     Raises
     ------
     ValueError
-        If `classyfire_levels` are not a subset of ['kingdom', 'superclass',
-        'class', 'subclass', 'direct_parent']
-    ValueError
         If feature data does not contain the column 'smiles'
+
     Returns
     -------
     pd.DataFrame
-        feature data table that contains Classyfire annotations per
-        mass-spec feature
+        feature data table with Classyfire annotations ('kingdom',
+        'superclass', 'class','subclass', 'direct_parent') per mass-spec
+        feature
     '''
-    if classyfire_levels is None:
-        classyfire_levels = ['kingdom', 'superclass', 'class',
-                             'subclass', 'direct_parent']
-    if not set(classyfire_levels).issubset({'kingdom', 'superclass', 'class',
-                                            'subclass', 'direct_parent'}):
-        raise ValueError('One or more Classyfire taxonomic levels are invalid')
+    classyfire_levels = ['kingdom', 'superclass', 'class', 'subclass',
+                         'direct_parent']
     if 'smiles' not in feature_data.columns:
         raise ValueError('Feature data table must contain the column `smiles` '
                          'to run Classyfire')
     classyfire = {}
+    unexpected = {}
     for idx in feature_data.index:
         smiles = feature_data.loc[idx, 'smiles']
         if pd.notna(smiles):
@@ -54,19 +48,29 @@ def get_classyfire_taxonomy(feature_data: pd.DataFrame,
             inchikey = requests.get(url_smiles+smiles).text
             url_inchi = 'https://gnps-classyfire.ucsd.edu/entities/'
             response = requests.get(url_inchi+str(inchikey)+'.json')
-            if response.status_code != 404:
+            if response.status_code == 200:
                 response = response.json()
                 taxonomy = [response[level]['name']
                             if bool(response) and response[level] is not None
                             else 'unclassified'
                             for level in classyfire_levels]
                 classyfire[idx] = taxonomy
-            else:
+            elif response.status_code == 404:
                 classyfire[idx] = 'unclassified'
+            else:
+                classyfire[idx] = 'unexpected server response'
+                unexpected[smiles] = response.status_code
         else:
             classyfire[idx] = 'unclassified'
+    if bool(unexpected):
+        warnings.warn('The following structures (keys) produced unexpected '
+                      'server response codes (values):\n'+
+                      unexpected + '\n' + 'More information about '
+                      'response status codes can be found here:\n' +
+                      'https://www.ietf.org/assignments/http-status-codes/'
+                      'http-status-codes.txt')
     classyfire = pd.DataFrame(classyfire).T
     classyfire.columns = classyfire_levels
     classified_feature_data = pd.concat([feature_data, classyfire],
-                                        sort='False', axis=1)
+                                        sort=False, axis=1)
     return classified_feature_data
