2548ad8a412aa903b49538c6404768932a4395be
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-10-14 16:52:29 -0700

    add tests for classyfire function

---
+++ q2_qemistree/tests/test_classyfire.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2018, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+import os
+import qiime2
+import pandas as pd
+from q2_qemistree import get_classyfire_taxonomy
+
+
+class TestHierarchy(TestCase):
+    def setUp(self):
+        THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+        badfdata = os.path.join(THIS_DIR, 'data/feature_data_no_smiles.txt')
+        self.badfdata = pd.read_csv(badfdata, sep='\t')
+        self.badfdata.set_index('label')
+
+    def test_no_smiles(self):
+        msg = 'Feature data table must contain the column `smiles` '
+        'to run Classyfire'
+        with self.assertRaisesRegex(ValueError, msg):
+            get_classyfire_taxonomy(self.badfdata)
+
+
+if __name__ == '__main__':
+    main()
