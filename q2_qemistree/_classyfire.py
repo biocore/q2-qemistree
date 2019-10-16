952564edbc0f62245497c9e9f3f1026d61d171f1
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-10-16 13:21:42 -0700

    adding check to test smile --> inchikey conversion

--- q2_qemistree/_classyfire.py
+++ q2_qemistree/_classyfire.py
@@ -26,6 +26,7 @@ def get_classyfire_taxonomy(feature_data: pd.DataFrame) -> pd.DataFrame:
     ------
     ValueError
         If feature data does not contain the column 'smiles'
+        If all SMILES are NaNs
 
     Returns
     -------
@@ -39,13 +40,23 @@ def get_classyfire_taxonomy(feature_data: pd.DataFrame) -> pd.DataFrame:
     if 'smiles' not in feature_data.columns:
         raise ValueError('Feature data table must contain the column `smiles` '
                          'to run Classyfire')
+    if feature_data['smiles'].notna().sum() == 0:
+        raise ValueError("The column 'smiles' in feature data table "
+                         "should have at least one structural annotation "
+                         "to run Classyfire")
     classyfire = {}
-    unexpected = {}
+    no_inchikey = []
+    unexpected = []
     for idx in feature_data.index:
         smiles = feature_data.loc[idx, 'smiles']
         if pd.notna(smiles):
             url_smiles = 'https://gnps-structure.ucsd.edu/inchikey?smiles='
-            inchikey = requests.get(url_smiles+smiles).text
+            response = requests.get(url_smiles+smiles)
+            if response.status_code != 200:
+                classyfire[idx] = 'SMILE parse error'
+                no_inchikey.append((idx, smiles))
+                continue
+            inchikey = response.text
             url_inchi = 'https://gnps-classyfire.ucsd.edu/entities/'
             response = requests.get(url_inchi+str(inchikey)+'.json')
             if response.status_code == 200:
@@ -59,18 +70,22 @@ def get_classyfire_taxonomy(feature_data: pd.DataFrame) -> pd.DataFrame:
                 classyfire[idx] = 'unclassified'
             else:
                 classyfire[idx] = 'unexpected server response'
-                unexpected[smiles] = response.status_code
+                unexpected.append((idx, smiles, response.status_code))
         else:
             classyfire[idx] = 'unclassified'
+    if bool(no_inchikey):
+        warnings.warn('The following structures (id, SMILES) could not '
+                      'be parsed to generate InChIKey for Classyfire:\n' +
+                      str(no_inchikey), UserWarning)
     if bool(unexpected):
-        warnings.warn('The following structures (keys) produced unexpected '
-                      'server response codes (values):\n' +
-                      unexpected + '\n' + 'More information about '
-                      'response status codes can be found here:\n' +
+        warnings.warn('The following features produced unexpected '
+                      'server response codes (id, SMILES, response code):\n' +
+                      str(unexpected) + '\n' +
+                      'More information about response status codes can be '
+                      'found here:\n' +
                       'https://www.ietf.org/assignments/http-status-codes/'
                       'http-status-codes.txt')
-    classyfire = pd.DataFrame(classyfire).T
-    classyfire.columns = classyfire_levels
+    classyfire = pd.DataFrame(classyfire, index=classyfire_levels).T
     classified_feature_data = pd.concat([feature_data, classyfire],
                                         sort=False, axis=1)
     return classified_feature_data
