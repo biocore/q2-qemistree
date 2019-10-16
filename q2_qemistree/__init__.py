bbd19d3a0024eae4a3c596f27f4e83ab1161799b
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-10-16 13:26:13 -0700

    adding prune_hierarchy module to init

--- q2_qemistree/__init__.py
+++ q2_qemistree/__init__.py
@@ -12,13 +12,14 @@ from ._fingerprint import (compute_fragmentation_trees,
                            predict_fingerprints)
 from ._classyfire import get_classyfire_taxonomy
 from ._hierarchy import make_hierarchy
+from ._prune_hierarchy import prune_hierarchy
 from ._semantics import (MassSpectrometryFeatures, MGFDirFmt,
                          CSIFolder, CSIDirFmt, ZodiacFolder, ZodiacDirFmt,
                          SiriusFolder, SiriusDirFmt, OutputDirs)
 
 __all__ = ['compute_fragmentation_trees', 'rerank_molecular_formulas',
            'predict_fingerprints', 'make_hierarchy', 'get_classyfire_taxonomy',
-           'MassSpectrometryFeatures', 'MGFDirFmt',
+           'prune_hierarchy', 'MassSpectrometryFeatures', 'MGFDirFmt',
            'CSIFolder', 'CSIDirFmt', 'ZodiacFolder', 'ZodiacDirFmt',
            'SiriusFolder', 'SiriusDirFmt', 'OutputDirs']
 
