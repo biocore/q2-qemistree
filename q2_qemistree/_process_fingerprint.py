37585b23c6b5cdbaaa8cce6c7c387337a990e956
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2019-11-21 23:41:00 -0800

    remove idx when for loop removed

--- q2_qemistree/_process_fingerprint.py
+++ q2_qemistree/_process_fingerprint.py
@@ -68,7 +68,7 @@ def get_feature_smiles(csi_result: CSIDirFmt, collated_fps: pd.DataFrame,
     smiles['ms2_smiles'] = np.nan
     if ms2_match is not None:
         ms2_ids = ms2_match.index.intersection(smiles.index)
-        smiles.loc[idx, 'ms2_smiles'] = ms2_match.loc[ms2_ids, 'Smiles']
+        smiles.loc['ms2_smiles'] = ms2_match.loc[ms2_ids, 'Smiles']
     return smiles
 
 
