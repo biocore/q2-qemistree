d97df7d63249874c9aa7ace3373cd3aaa5253d34
Qiyun Zhu <qiyunzhu@gmail.com>
2020-01-20 19:33:35 +0000

    added iToL bar chart function (#91)
    * added iToL bar chart function
    
    * removed chemical Id mapping
    
    * resolve x2
    
    * resolve x3
    
    * updated docstring

--- q2_qemistree/_itol_metadata.py
+++ q2_qemistree/_itol_metadata.py
@@ -11,7 +11,8 @@ import warnings
 import pandas as pd
 import seaborn as sns
 import click
-from qiime2 import Artifact
+import biom
+from qiime2 import Artifact, Metadata
 
 
 def classyfire_to_colors(classified_feature_data: pd.DataFrame,
@@ -46,14 +47,28 @@ def classyfire_to_colors(classified_feature_data: pd.DataFrame,
 @click.option('--color-palette', default='bright', type=str,
               help='Color palette for tree clades. One of the options'
               ' allowed by seaborn.color_palette()')
+@click.option('--sample-feature-table', type=str,
+              help='Path to sample feature table.')
+@click.option('--sample-metadata', type=str,
+              help='Path to sample metadata.')
+@click.option('--sample-metadata-column', type=str,
+              help='Categorical sample metadata column.')
+@click.option('--barchart-file-path', default='./itol_bars.txt', type=str,
+              help='Path to file with values for multi-value bar chart')
 def get_itol_visualization(classified_feature_data: str,
                            feature_data_column: str = 'class',
                            ms2_label: bool = True,
                            color_file_path: str = './itol_colors.txt',
                            label_file_path: str = './itol_labels.txt',
-                           color_palette: str = 'husl'):
+                           color_palette: str = 'husl',
+                           sample_feature_table: str = None,
+                           sample_metadata: str = None,
+                           sample_metadata_column: str = None,
+                           barchart_file_path: str = './itol_bars.qza'):
     '''This function creates iTOL metadata files to specify clade colors and
-    tip labels based on Classyfire annotations.'''
+    tip labels based on Classyfire annotations. It also adds bar plots of the
+    abundance of features stratified by user-specified sample metadata column.
+    '''
     fdata = Artifact.load(classified_feature_data).view(pd.DataFrame)
     color_map = classyfire_to_colors(fdata, feature_data_column, color_palette)
     with open(color_file_path, 'w+') as fh:
@@ -84,6 +99,42 @@ def get_itol_visualization(classified_feature_data: str,
             for idx in fdata.index:
                 label = fdata.loc[idx, feature_data_column]
                 fh.write(idx + '\t' + label + '\n')
+
+    # generate bar chart
+    if barchart_file_path:
+        get_itol_barchart(fdata, sample_feature_table, sample_metadata,
+                          sample_metadata_column, barchart_file_path)
+
+
+def get_itol_barchart(fdata: pd.DataFrame,
+                      table_file: str,
+                      metadata_file: str,
+                      metadata_column: str,
+                      output_file: str):
+    '''Generate a table in QIIME 2 artifact format which can be directly
+    parsed by iTOL and yield a multi-bar chart.
+    '''
+    # load sample feature table
+    table = Artifact.load(table_file)
+
+    # extract BIOM table
+    table = table.view(biom.Table)
+
+    # load sample metadata
+    meta = Metadata.load(metadata_file)
+
+    # generate a sample Id to category map
+    column = meta.get_column(metadata_column).drop_missing_values()
+    catmap = column.to_series().to_dict()
+
+    # collapse feature table by category
+    # note: when multiple samples map to one category, take **mean**
+    table = table.collapse(lambda i, _: catmap[i], norm=True,
+                           axis='sample')
+
+    # import BIOM table into QIIME 2 and save
+    res = Artifact.import_data('FeatureTable[Frequency]', table)
+    res.save(output_file)
 
 
 if __name__ == '__main__':
