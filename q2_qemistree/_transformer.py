1d26d254dec49093eceef18370329e37eab4188f
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2020-02-26 13:03:44 -0800

    add clarifying comment

--- q2_qemistree/_transformer.py
+++ q2_qemistree/_transformer.py
@@ -4,15 +4,10 @@ import pandas as pd
 import qiime2
 
 
-def _read_dataframe(fh, index: str=None):
+def _read_dataframe(fh):
     # Using `dtype=object` and `set_index` to avoid type casting/inference
     # of any columns or the index.
     df = pd.read_csv(fh, sep='\t', header=0, dtype='str')
-    if index == None:
-        df.set_index(df.columns[0], drop=True, append=False, inplace=True)
-    else:
-        df.set_index(index, drop=True, append=False, inplace=True)
-    df.index.name = 'id'
     return df
 
 # define a transformer from pd.DataFrame to -> TSVMolecules
@@ -27,7 +22,15 @@ def _1(data: pd.DataFrame) -> TSVMolecules:
 @plugin.register_transformer
 def _2(ff: TSVMolecules) -> pd.DataFrame:
     with ff.open() as fh:
-        df = _read_dataframe(fh, index='cluster index')
+        df = _read_dataframe(fh)
+        # Using 'cluster index' as index explicity for library matches
+        # since it may not be the first column
+        if 'cluster index' in df.columns:
+            df.set_index('cluster index', drop=True, append=False,
+                         inplace=True)
+        else:
+            df.set_index(df.columns[0], drop=True, append=False, inplace=True)
+        df.index.name = 'id'
         return df
 
 # define a transformer from TSVMolecules -> qiime2.Metadata
