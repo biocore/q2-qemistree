e636780c32f7f9d8fd31a16f990b1b056278051c
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2020-04-15 00:31:24 -0700

    ENH: Add validation for MGF files
    Fixes #50

--- q2_qemistree/_semantics.py
+++ q2_qemistree/_semantics.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016-2018, QIIME 2 development team.
+# Copyright (c) 2017-2018, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
@@ -12,10 +12,61 @@ from q2_types.feature_data import FeatureData
 import os
 
 
+def validate_mgf(iterable):
+    observed_ids = set([])
+
+    # ms1 and ms2 count the number of records for the MS1 and MS2 spectra
+    read_id, new_feature_id, ms1, ms2 = None, None, 0, 0
+
+    for line in iterable:
+        if line.startswith('FEATURE_ID='):
+            # get the feature identifier without the new line
+            new_feature_id = line.split('=')[1].strip()
+
+            if read_id is None:
+                read_id = new_feature_id
+            else:
+                if new_feature_id != read_id:
+                    if ms2 < 1:
+                        raise ValueError('Feature "%s" does not have at least '
+                                         'one MSLEVEL=2 record' % read_id)
+                    if new_feature_id in observed_ids:
+                        raise ValueError('Feature "%s" has repeated records, '
+                                         'all records for each feature are '
+                                         'assumed to be listed contiguously.'
+                                         % new_feature_id)
+
+                    # if the previous record is good, then we'll reset
+                    # the current state variables and start over
+                    observed_ids.add(new_feature_id)
+                    read_id = new_feature_id
+
+                    ms1, ms2 = 0, 0
+
+        elif read_id is not None:
+            if line.startswith('MSLEVEL=1'):
+                ms1 += 1
+
+                if ms1 > 1:
+                    raise ValueError('Feature "%s" has more than one MSLEVEL=1'
+                                     ' record' % read_id)
+                observed_ids.add(read_id)
+            elif line.startswith('MSLEVEL=2'):
+                ms2 += 1
+            else:
+                # we ignore all lines that are not feature identifiers
+                # or ms level values
+                pass
+    return True
+
+
 class MGFFile(model.TextFileFormat):
     def sniff(self):
-        # we don't really parse this file
-        return True
+        # Something basic that checks for unique MS1s and at least one MS2 per
+        # MS1 should help us weed out a large portion of incorrect MGF file
+        # relatively easily.
+        with open(str(self)) as f:
+            return validate_mgf(f)
 
 
 MGFDirFmt = model.SingleFileDirectoryFormat('MGFFile', 'features.mgf', MGFFile)
