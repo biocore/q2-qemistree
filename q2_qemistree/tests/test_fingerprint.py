fe26289f908f3f1c4dd86641a8708bd46a13d796
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2020-04-15 18:02:19 -0700

    BUG: Fix problem with sirius

--- q2_qemistree/tests/test_fingerprint.py
+++ q2_qemistree/tests/test_fingerprint.py
@@ -85,15 +85,11 @@ class FingerprintTests(TestCase):
                                            fragmentation_trees=sirout,
                                            features=ions)
         contents = os.listdir(result.get_path())
-        print(contents)
-        # self.assertTrue(('zodiac_summary.csv' in contents))
+        self.assertTrue(('zodiac_summary.csv' in contents))
 
         contents = os.listdir(result.path)
         self.assertTrue(('stderr.txt' in contents))
         self.assertTrue(('stdout.txt' in contents))
-
-        with open(os.join(result.path, 'stderr.txt')) as f:
-            print(f.read())
 
     def test_fingerid(self):
         zodout = self.zodout.view(ZodiacDirFmt)
