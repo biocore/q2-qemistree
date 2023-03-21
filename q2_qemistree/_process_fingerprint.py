79ec67a348b92fe30d9814073b07962c81f82723
helenamrusso <55901470+helenamrusso@users.noreply.github.com>
2023-03-20 23:07:10 -0700

    Update _process_fingerprint.py

--- q2_qemistree/_process_fingerprint.py
+++ q2_qemistree/_process_fingerprint.py
@@ -10,51 +10,67 @@ import os
 import pandas as pd
 import numpy as np
 import pkg_resources
+import zipfile
 
-from ._semantics import CSIDirFmt
+from ._semantics import SiriusDirFmt
 
 
 data = pkg_resources.resource_filename('q2_qemistree', 'data')
 
 
-def collate_fingerprint(csi_result: CSIDirFmt,
+def collate_fingerprint(csi_result: SiriusDirFmt,
                         metric: str = 'euclidean'):
     '''
     This function collates predicted chemical fingerprints for mass-spec
     features in an experiment.
     '''
-    if isinstance(csi_result, CSIDirFmt):
+    if isinstance(csi_result, SiriusDirFmt):
         csi_result = str(csi_result.get_path())
-    fpfoldrs = os.listdir(csi_result)
+    df_csi_result = pd.read_csv(os.path.join(csi_result, 'compound_identifications.tsv'), sep='\t')
+    df_csi_result.set_index(df_csi_result['id'], inplace=True)
+    fpfoldrs = list(df_csi_result.index) # na verdade pegamos os nomes das pastas a partir do df
     molfp = dict()
+
     for foldr in fpfoldrs:
         if os.path.isdir(os.path.join(csi_result, foldr)):
             fid = foldr.split('_')[-1]
-            fidpath = os.path.join(csi_result, foldr)
-            if 'fingerprints' in os.listdir(fidpath):
-                fname = os.listdir(os.path.join(fidpath, 'fingerprints'))[0]
-                with open(os.path.join(fidpath, 'fingerprints', fname)) as f:
+            fidpath = os.path.join(csi_result, foldr)  # é uma pasta, esse é o caminho
+            if 'fingerprints' in os.listdir(fidpath):  # tem arquivos com nome fingerprints dentro da pasta?
+                with zipfile.ZipFile(os.path.join(fidpath, 'fingerprints'), 'r') as zip_ref:
+                    zip_ref.extractall(os.path.join(fidpath, 'fingerprints_extracted'))
+                fname = df_csi_result.loc[foldr]['molecularFormula'] + '_' \
+                        + df_csi_result.loc[foldr]['adduct'].replace(' ', '') + '.fpt' # novo
+                print(fname) # debug
+                with open(os.path.join(fidpath, 'fingerprints_extracted', fname)) as f:
                     fp = f.read().strip().split('\n')
                 molfp[fid] = [float(val) for val in fp]
+
     collated_fps = pd.DataFrame.from_dict(molfp, orient='index')
     if metric == 'jaccard':
         collated_fps = (collated_fps > 0.5).astype(int)
     if collated_fps.shape == (0, 0):
         raise ValueError('Fingerprint file is empty!')
-    substructrs = pd.read_csv(os.path.join(csi_result, 'csi_fingerid.tsv'),
+
+    check_neg = pd.read_csv(os.path.join(csi_result, 'canopus_compound_summary.tsv'),
+                            dtype=str, sep='\t')
+    if check_neg['adduct'][0][-1] == '+':
+        substructrs = pd.read_csv(os.path.join(csi_result, 'csi_fingerid.tsv'),
                               index_col='relativeIndex', dtype=str, sep='\t')
+    else:
+        substructrs = pd.read_csv(os.path.join(csi_result, 'csi_fingerid_neg.tsv'),
+                                  index_col='relativeIndex', dtype=str, sep='\t')
     collated_fps.index.name = '#featureID'
     collated_fps.columns = substructrs.loc[collated_fps.columns,
                                            'absoluteIndex']
     return collated_fps
 
 
-def get_feature_smiles(csi_result: CSIDirFmt, collated_fps: pd.DataFrame,
+def get_feature_smiles(csi_result: SiriusDirFmt, collated_fps: pd.DataFrame,
                        library_match: pd.DataFrame = None):
     '''This function gets the SMILES of mass-spec features from
     CSI:FingerID and optionally, MS/MS library match results
     '''
-    if isinstance(csi_result, CSIDirFmt):
+    if isinstance(csi_result, SiriusDirFmt):
         csi_result = str(csi_result.get_path())
     csi_summary = os.path.join(csi_result, 'compound_identifications.tsv')
     csi_summary = pd.read_csv(csi_summary, dtype=str,
@@ -78,7 +94,7 @@ def get_feature_smiles(csi_result: CSIDirFmt, collated_fps: pd.DataFrame,
     return smiles
 
 
-def process_csi_results(csi_result: CSIDirFmt,
+def process_csi_results(csi_result: SiriusDirFmt,
                         library_match: pd.DataFrame = None,
                         metric: str = 'euclidean') -> (pd.DataFrame,
                                                        pd.DataFrame):
