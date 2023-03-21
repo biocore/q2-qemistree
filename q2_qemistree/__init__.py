20730147f84a6c5aa70abb3d6fa56875fd53feb7
helenamrusso <55901470+helenamrusso@users.noreply.github.com>
2023-03-20 22:49:20 -0700

    Update __init__.py

--- q2_qemistree/__init__.py
+++ q2_qemistree/__init__.py
@@ -7,20 +7,14 @@
 # ----------------------------------------------------------------------------
 from ._version import get_versions
 
-from ._fingerprint import (compute_fragmentation_trees,
-                           rerank_molecular_formulas,
-                           predict_fingerprints)
-from ._classyfire import get_classyfire_taxonomy
+from ._fingerprint import (compute_fingerprint_classes)
 from ._hierarchy import make_hierarchy
 from ._prune_hierarchy import prune_hierarchy
 from ._semantics import (MassSpectrometryFeatures, MGFDirFmt,
-                         CSIFolder, CSIDirFmt, ZodiacFolder, ZodiacDirFmt,
                          SiriusFolder, SiriusDirFmt, OutputDirs)
 
-__all__ = ['compute_fragmentation_trees', 'rerank_molecular_formulas',
-           'predict_fingerprints', 'make_hierarchy', 'get_classyfire_taxonomy',
+__all__ = ['compute_fingerprint_classes', 'make_hierarchy',
            'prune_hierarchy', 'plot', 'MassSpectrometryFeatures', 'MGFDirFmt',
-           'CSIFolder', 'CSIDirFmt', 'ZodiacFolder', 'ZodiacDirFmt',
            'SiriusFolder', 'SiriusDirFmt', 'OutputDirs']
 
 __version__ = get_versions()['version']
